13291cb0f99a0486301a89811517c2c6173de2bf
Zero_System <zero@system.lc>
2018-04-26 16:44:24 -0600

    First Push

---
+++ cmake-build-debug/CMakeFiles/FlowSensor.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Zero_System/Documents/Arduino/FlowSensor/lib/FlowSensor/FlowSensor.cpp" "C:/Users/Zero_System/Documents/Arduino/FlowSensor/cmake-build-debug/CMakeFiles/FlowSensor.dir/lib/FlowSensor/FlowSensor.cpp.obj"
+  "C:/Users/Zero_System/Documents/Arduino/FlowSensor/src/FlowSensorTest.cpp" "C:/Users/Zero_System/Documents/Arduino/FlowSensor/cmake-build-debug/CMakeFiles/FlowSensor.dir/src/FlowSensorTest.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/cores/arduino"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/variants/eightanaloginputs"
+  "../src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/LiquidCrystal/src"
+  "../lib/FlowSensor"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/__cores__/arduino/EEPROM/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/__cores__/arduino/HID/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/__cores__/arduino/SPI/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/__cores__/arduino/SoftwareSerial/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/__cores__/arduino/Wire/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/Adafruit_CircuitPlayground"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/Adafruit_CircuitPlayground/utility"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/Bridge/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/Esplora/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/Ethernet/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/Firmata"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/Firmata/utility"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/GSM/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/Keyboard/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/Mouse/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/RobotIRremote/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/Robot_Control/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/Robot_Motor/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/SD/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/Servo/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/SpacebrewYun/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/Stepper/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/TFT/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/Temboo/src"
+  "C:/Users/Zero_System/.platformio/packages/framework-arduinoavr/libraries/WiFi/src"
+  "C:/Users/Zero_System/.platformio/packages/toolchain-atmelavr/avr/include"
+  "C:/Users/Zero_System/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/4.9.2/include"
+  "C:/Users/Zero_System/.platformio/packages/toolchain-atmelavr/lib/gcc/avr/4.9.2/include-fixed"
+  "C:/Users/Zero_System/.platformio/packages/tool-unity"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/FlowSensor.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FlowSensor.dir/src/FlowSensorTest.cpp.obj"
+  "CMakeFiles/FlowSensor.dir/lib/FlowSensor/FlowSensor.cpp.obj"
+  "FlowSensor.pdb"
+  "FlowSensor.exe"
+  "FlowSensor.exe.manifest"
+  "libFlowSensor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FlowSensor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
